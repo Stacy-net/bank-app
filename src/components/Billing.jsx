7961745a6484b80332d6928fced9f7475b589730
Anastasiia Bielik <ana.stasy0112@gmail.com>
2023-07-31 21:25:59 +0300

    added CardDeal component

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -12,6 +12,15 @@ const Billing = () => (
 				className="w-[100%] h-[100%] realtive z-[5]"
 			/>
 			<div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+			<div className="absolute z-[0] -left-1/2 bpttom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
+		</div>
+		<div className={layout.sectionInfo}>
+			<h2 className={styles.heading2}>Easily control your <br className='sm:block hidden'/>billing & invoicing.</h2>
+			<p className={`${styles.paragraph} max-w-[470px] mt-5`}>Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.</p>
+			<div className='flex flex-row flex-wrap sm:mt-10 mt-6'>
+				<img src={apple} alt="apple_store" className='w-[128px] h-[42px] object-contain mr-5 cursor-pointer'/>
+				<img src={google} alt="google_play" className='w-[128px] h-[42px] object-contain mr-5 cursor-pointer'/>
+			</div>
 		</div>
 	</section>
 );
